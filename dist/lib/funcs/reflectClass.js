1f1883c565fca6d4b2b78c9f35c02e63defddf71
ranyunlong <849291425@qq.com>
2020-05-09 00:17:29 +0800

    update

--- dist/lib/funcs/reflectClass.js
+++ dist/lib/funcs/reflectClass.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("../../index");
+const classReflects = new Map();
 function reflectClass(target) {
-    return new index_1.ClassReflect(target);
+    // 添加缓存处理
+    return classReflects.get(target) || new index_1.ClassReflect(target);
 }
 exports.reflectClass = reflectClass;
--- src/lib/funcs/reflectClass.ts
+++ src/lib/funcs/reflectClass.ts
@@ -1,6 +1,9 @@
 import {ClassReflect} from "../../index";
 import {BaseConstructor} from "../../interface";
 
+const classReflects = new Map<BaseConstructor, ClassReflect<any>>();
+
 export function reflectClass<T extends BaseConstructor>(target: T): ClassReflect<T> {
-  return new ClassReflect<T>(target);
+  // 添加缓存处理
+  return classReflects.get(target) || new ClassReflect<T>(target);
 }
