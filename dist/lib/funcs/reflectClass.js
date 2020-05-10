14b5b47167cbc10b81dd538bcc69ea468ac896eb
leo-ran <service@ranyunlong.com>
2020-05-10 18:35:07 +0800

    update

--- dist/lib/funcs/reflectClass.js
+++ dist/lib/funcs/reflectClass.js
@@ -4,7 +4,7 @@ const index_1 = require("../../index");
 const classReflects = new Map();
 function reflectClass(target, parent) {
     // 添加缓存处理
-    const classReflect = classReflects.get(target) || parent ? new index_1.ClassReflect(target, parent) : new index_1.ClassReflect(target);
+    const classReflect = classReflects.get(target) || (parent ? new index_1.ClassReflect(target, parent) : new index_1.ClassReflect(target));
     classReflects.set(target, classReflect);
     return classReflect;
 }
--- src/lib/funcs/reflectClass.ts
+++ src/lib/funcs/reflectClass.ts
@@ -5,7 +5,7 @@ const classReflects = new Map<BaseConstructor, ClassReflect<any>>();
 
 export function reflectClass<T extends BaseConstructor>(target: T, parent?: ClassReflect): ClassReflect<T> {
   // 添加缓存处理
-  const classReflect = classReflects.get(target) || parent ? new ClassReflect<T>(target, parent) : new ClassReflect<T>(target);
+  const classReflect = classReflects.get(target) || (parent ? new ClassReflect<T>(target, parent) : new ClassReflect<T>(target));
   classReflects.set(target, classReflect);
   return classReflect;
 }
