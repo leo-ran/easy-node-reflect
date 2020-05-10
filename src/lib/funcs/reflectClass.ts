7971056a055d2b2598bcb752f5f5e3ecb9d56006
leo-ran <service@ranyunlong.com>
2020-05-10 11:49:38 +0800

    update

--- src/lib/funcs/reflectClass.ts
+++ src/lib/funcs/reflectClass.ts
@@ -3,7 +3,7 @@ import {BaseConstructor} from "../../interface";
 
 const classReflects = new Map<BaseConstructor, ClassReflect<any>>();
 
-export function reflectClass<T extends BaseConstructor>(target: T, parent: ClassReflect): ClassReflect<T> {
+export function reflectClass<T extends BaseConstructor>(target: T, parent?: ClassReflect): ClassReflect<T> {
   // 添加缓存处理
   return classReflects.get(target) || parent ? new ClassReflect<T>(target, parent) : new ClassReflect<T>(target);
 }
